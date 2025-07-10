8c6d18abaffa1d31d916c2a7191a0050049f2041
Matt_Leffa <83082324+MatheusLeffa@users.noreply.github.com>
2025-07-10 02:10:24 -0300

    updates

---
+++ FrontEnd/src/main/java/com/senai/frontend/SenaiBetFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.senai.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaiBetFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaiBetFrontEndApplication.class, args);
+	}
+
+}
---
+++ FrontEnd/src/test/java/com/senai/frontend/SenaiBetFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiBetFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
